7b8fdab28ba3bee2eb10d6cda702149538503c60
nfi002 <nabeela.f.ibrahim@gmail.com>
2023-02-23 15:18:58 -0500

    add support for uids

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export const main: HttpFunction = async (req, res) => {
     res.set("Access-Control-Allow-Origin", "*")
 
     try {
-        if (!("barcode" in req.query)) {
+        if (!("barcode" in req.query || "uid" in req.query) || ("barcode" in req.query && "uid" in req.query)) {
             console.log("barcode not provided in query")
             res.status(400)
             res.json({ error: "barcode value not provided" })
@@ -15,14 +15,19 @@ export const main: HttpFunction = async (req, res) => {
         const client = ldap.createClient({ url: process.env.LDAP_URL as string })
         const password = process.env.LDAP_PASSWORD!.replaceAll("\n", "")
         await client.bind(process.env.LDAP_DN as string, password)
+        
         console.log("client binded")
 
-        const barcode = req.query["barcode"]
-        console.log("looking up barcode: ", barcode)
-        const results = await client.searchReturnAll("ou=people,dc=umd,dc=edu", {
-            filter: `(&(objectClass=*)(umLibraryBarcode=${barcode}))`,
-            scope: "sub",
-        })
+        const results = "barcode" in req.query ? 
+            (await client.searchReturnAll("ou=people,dc=umd,dc=edu", {
+                filter: `(&(objectClass=*)(umLibraryBarcode=${req.query["barcode"]}))`,
+                scope: "sub",
+            })) :
+            (await client.searchReturnAll("ou=people,dc=umd,dc=edu", {
+                filter: `(&(objectClass=*)(employeeNumber=${req.query["uid"]}))`,
+                scope: "sub",
+            }))
+        
         console.log("barcode lookup results: ", results)
         return res.json({ results })
     } catch (error) {
